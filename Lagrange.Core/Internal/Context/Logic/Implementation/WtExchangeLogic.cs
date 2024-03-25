ff99bb482757cab27e71ce461d22b1192acf068e
pk5ls20 <pk5ls20@outlook.com>
2024-03-25 13:36:21 +0800

    [Core] Unelegant `KickNTEvent` post-offline implementation

--- Lagrange.Core/Internal/Context/Logic/Implementation/WtExchangeLogic.cs
+++ Lagrange.Core/Internal/Context/Logic/Implementation/WtExchangeLogic.cs
@@ -52,6 +52,7 @@ internal class WtExchangeLogic : LogicBase
                 
                 Collection.Invoker.PostEvent(new BotOfflineEvent(kick.Tag, kick.Message)); // TODO: Fill in the reason of offline
                 Collection.Scheduler.Dispose();
+                Environment.Exit(114514);
                 break;
         }
     }
